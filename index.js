5fb40051f8f7d046fb2a4e1b901f31b07d026345
md-mh <mmahedi49@gmail.com>
2022-02-22 09:17:42 +0600

    test

--- index.js
+++ index.js
@@ -1,105 +1,139 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const cors = require("cors");
-require("dotenv").config();
-const { MongoClient } = require("mongodb");
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 
-const employeesHandler = require("./routeHandler/employeesHandler");
+
+const app = express();
 const port = process.env.PORT || 5000;
 
+
+//Middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.o0i8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vw8cq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority1`;
 
-mongoose
-    .connect(uri, {
-        useNewUrlParser: true, useUnifiedTopology: true,
-        dbName: 'hr-care'
-    })
-    .then(() => console.log("connection successful"))
-    .catch((err) => console.log(err));
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// async function run() {
-//     try {
-//         await client.connect();
-//         const usersCollection = client.db("hr-care").collection("user");
-//         const employeeRequestCollection = client.db("hr-care").collection("employeeRequest");
+async function run() {
+    try {
 
-//         //User Post API
-//         app.post("/users", async (req, res) => {
-//             const user = req.body;
-//             const result = await usersCollection.insertOne(user);
-//             res.send(result);
-//             console.log(user);
-//         });
-//         //  make admin
-//         app.put("/makeAdmin", async (req, res) => {
-//             const filter = { email: req.body.email };
-//             const result = await usersCollection.find(filter).toArray();
-//             if (result) {
-//                 const documents = await usersCollection.updateOne(filter, {
-//                     $set: { role: "admin" },
-//                 });
-//                 console.log(documents);
-//             }
-//         });
-
-//         // check admin or not
-//         app.get("/checkAdmin/:email", async (req, res) => {
-//             const result = await usersCollection
-//                 .find({ email: req.params.email })
-//                 .toArray();
-//             console.log(result);
-//             res.send(result);
-//         });
-
-//         //Employee Request Get API
-//         app.get("/employeeRequest", async (req, res) => {
-//             const getemployeeRequest = employeeRequestCollection.find({});
-//             const result = await getemployeeRequest.toArray();
-//             res.send(result);
-//             console.log(getemployeeRequest);
-//         });
-
-//         //Employee Request Post API
-//         app.post("/employeeRequest", async (req, res) => {
-//             const employeeRequest = req.body;
-//             const result = await employeeRequestCollection.insertOne(employeeRequest);
-//             res.send(result);
-//             console.log(employeeRequest);
-//         });
-//     } finally {
-//         // await  client.close();
-//     }
-// }
-
-// run().catch(console.dir);
-
-// application routes
-
-app.use("/employees", employeesHandler);
+        await client.connect();
+        const database = client.db('TourBlog');
+        const usercollection = database.collection('user');
+        const tourcollection = database.collection('tour');
 
 
-// default error handler
-const errorHandler = (err, req, res, next) => {
-    if (res.headersSent) {
-        return next(err);
+        // GET Admin API
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const getEmail = { email: email };
+            const user = await usercollection.findOne(getEmail);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        // POST User API
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const result = await usercollection.insertOne(user);
+            console.log(result)
+            res.json('result');
+        })
+
+        //PUT User API
+        app.put('/user', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usercollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // PUT Admin API
+        app.put('/user/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: { role: user.role }
+            }
+            const result = await usercollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        })
+
+
+        // GET tour API
+        app.get('/tour', async (req, res) => {
+            const getdata = tourcollection.find({});
+            const showdata = await getdata.toArray();
+            res.send(showdata);
+        })
+
+        // GET Single tour API
+        app.get('/tour/:id', async (req, res) => {
+            const id = req.params.id;
+            const getId = { _id: ObjectId(id) };
+            const showId = await tourcollection.findOne(getId);
+            res.json(showId);
+        })
+
+
+        // POST tour API
+        app.post('/tour', async (req, res) => {
+            const add = req.body;
+            const result = await tourcollection.insertOne(add);
+            console.log(result);
+            res.json(result);
+        })
+
+        // DELETE tour API
+        app.delete('/tour/:id', async (req, res) => {
+            const id = req.params.id;
+            const getId = { _id: ObjectId(id) };
+            const deleteId = await tourcollection.deleteOne(getId);
+            res.json(deleteId);
+        })
+
+        //UPDATE tour API
+        app.put('/tour/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedTour = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const update = {
+                $set: {
+                    name: updatedTour.name, email: updatedTour.email, status: updatedTour.status, title: updatedTour.title, img: updatedTour.img, cost: updatedTour.cost, category: updatedTour.category, details: updatedTour.details, location: updatedTour.location
+                },
+            };
+            const result = await tourcollection.updateOne(filter, update, options);
+            res.json(result);
+        })
+
+
     }
-    res.status(500).json({ error: err });
+    finally {
+        // await client.close();
+    }
 }
+run().catch(console.dir);
 
-app.use(errorHandler);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
+app.get('/', (req, res) => {
+    res.send('Running Server...')
+})
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    console.log("Running port", port)
+})
+
+
+
+
+
+
